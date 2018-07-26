68d3208f8e0359f374970ec46d88def629cc072f
Ekanshi Agrawal <ekanshi@gmail.com>
2018-07-26 12:05:32 +0530

    Optimize task elements creation

--- script.js
+++ script.js
@@ -13,81 +13,59 @@ task.addEventListener('keypress', (event) => {
     var tasks = document.getElementById('tasks')
     var newDiv = document.createElement('div')
     let taskItems = []
-    var newTaskItem = document.createElement('input')
-    newTaskItem.type = 'checkbox'
-    newTaskItem.value = task.value
-    newTaskItem.id = 'taskCheck'
-    taskItems.push(newTaskItem)
+
+    var newTaskItem = createCustomElement('input', 'checkbox', 'taskCheck', task.value, null)
+ 	taskItems.push(newTaskItem)
 
     newTaskItem.onclick = function () {
   		var doneTasksList = document.getElementById('doneTasks')
   		for (let i = 0; i < newDiv.childNodes.length - 1; i++) {
+  			if (i === 2) {
+  				continue
+  			}
   			newDiv.childNodes[i].disabled = true
   		}
   		doneTasksList.appendChild(newDiv)
   	}
 
-    var newTaskLabel = document.createElement('label')
-    newTaskLabel.textContent = task.value
-    newTaskLabel.htmlFor = 'taskCheck'
+    var newTaskLabel = createCustomLabel(task.value, 'taskCheck')
     taskItems.push(newTaskLabel)
 
-    var optionButton = document.createElement('button')
-    optionButton.type = 'button'
-    optionButton.textContent = 'Options'
-    optionButton.classList.add = 'options'
+    var optionButton = createCustomElement('button', 'button', 'options', null, '^')
     taskItems.push(optionButton)
 
-    var noteLabel = document.createElement('label')
-    noteLabel.textContent = 'Notes'
-    noteLabel.htmlFor = 'note'
-    noteLabel.classList.add = 'options'
+    var noteLabel = createCustomLabel('Notes', 'note')
     noteLabel.hidden = true
     taskItems.push(noteLabel)
-    var taskNote = document.createElement('textarea')
-    taskNote.id = 'note'
-    taskNote.classList.add = 'options'
+
+    var taskNote = createCustomElement('textarea', null, 'note')
     taskNote.hidden = true
     taskItems.push(taskNote)
 
-   	var dueDateLabel = document.createElement('label')
-   	dueDateLabel.textContent = 'Due Date'
-   	dueDateLabel.htmlFor = 'taskDueBy'
-   	dueDateLabel.classList.add = 'options'
+   	var dueDateLabel = createCustomLabel('Due Date', 'taskDueBy')
    	dueDateLabel.hidden = true
    	taskItems.push(dueDateLabel)
-   	var dueDate = document.createElement('input')
-   	dueDate.type = 'date'
-   	dueDate.id = 'taskDueBy'
-   	dueDate.classList.add = 'options'
+
+   	var dueDate = createCustomElement('input', 'date', 'taskDueBy')
    	dueDate.hidden = true
    	taskItems.push(dueDate)
 
-   	var priorityLabel = document.createElement('label')
-   	priorityLabel.textContent = 'Priority'
-   	priorityLabel.htmlFor = 'Priority'
-   	priorityLabel.classList.add = 'options'
+   	var priorityLabel = createCustomLabel('Priority', 'Priority')
    	priorityLabel.hidden = true
    	taskItems.push(priorityLabel)
-   	var prioritySelect = document.createElement('select')
-   	prioritySelect.id = 'Priority'
-   	prioritySelect.classList.add = 'options'
+
+   	var prioritySelect = createCustomElement('select', null, 'Priority')
    	prioritySelect.hidden = true
    	taskItems.push(prioritySelect)
-   	var lowPrior = document.createElement('option')
-   	lowPrior.textContent = 'Low'
-   	var medPrior = document.createElement('option')
-   	medPrior.textContent = 'Medium'
-   	var highPrior = document.createElement('option')
-   	highPrior.textContent = 'High'
-   	prioritySelect.appendChild(lowPrior)
-   	prioritySelect.appendChild(medPrior)
-   	prioritySelect.appendChild(highPrior)
 
-   	var deleteButton = document.createElement('button')
-   	deleteButton.type = 'button'
-   	deleteButton.textContent = 'Delete'
-   	deleteButton.classList.add = 'options'
+   	var lowPriorOption = createCustomElement('option', null, null, null, 'Low')
+   	var medPriorOption = createCustomElement('option', null, null, null, 'Medium')
+   	var highPriorOption = createCustomElement('option', null, null, null, 'High')
+   	prioritySelect.appendChild(lowPriorOption)
+   	prioritySelect.appendChild(medPriorOption)
+   	prioritySelect.appendChild(highPriorOption)
+
+   	var deleteButton = createCustomElement('button', 'button', null, null, 'Delete')
    	deleteButton.hidden = true
    	taskItems.push(deleteButton)
 
@@ -121,6 +99,28 @@ task.addEventListener('keypress', (event) => {
 
     tasks.appendChild(newDiv)
 
+    let newTask = {}
+    newTask['des'] = task.value
+    newTask['class'] = 'left'
+    newTask['Priority'] = task.Priority
+    allTasks.push(newTask)
+
     task.value = ''
   }
 })
+
+function createCustomElement (ele, type, id, value, textContent) {
+  var element = document.createElement(ele)
+  if (type) element.type = type
+  if (value) element.value = value
+  if (textContent) element.textContent = textContent
+  if (id) element.id = id
+  return element
+}
+
+function createCustomLabel (textContent, ele) {
+  var label = document.createElement('label')
+  label.textContent = textContent
+  label.htmlFor = ele
+  return label
+}
